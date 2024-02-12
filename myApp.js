42473d6f6add3aebbf54c9851e6e5794736ef62e
MariusBaldovin <youremailid@example.com>
2024-02-12 21:41:56 +0000

    added uppercase in .env  /json endpoint  in myApp.js

--- myApp.js
+++ myApp.js
@@ -1,23 +1,43 @@
+var express = require("express");
 require("dotenv").config();
+var app = express();
+process.env.MESSAGE_STYLE == "uppercase";
+console.log(process.env.MESSAGE_STYLE);
 
-let express = require("express");
-let app = express();
+//let app = express();
+//console.log(process.env//.VARIABLE_ONE)
 
-app.get("/", (req, res) => {
+//app.use('/pics', express.static(__dirname + '/images'));
+//console.log("Hello World");
+/** app.get("/", (req,res)=>{
+  res.send("Hello Express");
+
+});*/
+
+// app.get("/",function(req,res){
+//res.sendFile(__dirname + "/views/index.html");
+//});
+
+app.use(express.static(__dirname + "/public"));
+
+//serve json on a specific route
+var message = { message: "Hello json" };
+//app.get('/json',(request,response)=> {
+//response.json(message);
+//});
+
+/**6)use the .env File to configure app*/
+
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
+  app.use(express.static(__dirname + "/public"));
 });
-
-app.use("/public", express.static(__dirname + "/public"));
-
-if (process.env.MESSAGE_STYLE === "uppercase") {
-  let response = "Hello json".toUpperCase();
-  app.get("/json", function (req, res) {
-    res.json({ message: response });
-  });
-} else {
-  app.get("/json", function (req, res) {
-    res.json({ message: "Hello json" });
-  });
-}
-
+//7.use the .env file
+app.get("/json", (request, response) => {
+  if (process.env.MESSAGE_STYLE == uppercase) {
+    response.json({ message: "HELLO JSON" });
+  } else {
+    response.json(message);
+  }
+});
 module.exports = app;
