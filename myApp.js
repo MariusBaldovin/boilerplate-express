c3ae4d8925aa67f38652727a743aa0d2bf727cfb
MariusBaldovin <youremailid@example.com>
2024-02-12 21:23:07 +0000

    added uppercase in .env  /json endpoint  in myApp.js

--- myApp.js
+++ myApp.js
@@ -9,10 +9,12 @@ app.get("/", (req, res) => {
 
 app.use("/public", express.static(__dirname + "/public"));
 
-var response = "Hello World".toUpperCase();
-
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  var response = "Hello World";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
+  }
+  res.json({ message: response });
 });
 
 module.exports = app;
