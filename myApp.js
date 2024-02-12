52a60407511d73e2691e4b150a5dc80757aa174c
MariusBaldovin <youremailid@example.com>
2024-02-12 22:14:30 +0000

    added root date middleware  in myApp.js

--- myApp.js
+++ myApp.js
@@ -10,6 +10,19 @@ app.use((req, res, next) => {
   next(); // Pass control to the next middleware/route handler
 });
 
+// Middleware function that adds the current time to req.time
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString(); // Add the current time to the request
+    next(); // Pass control to the next middleware/handler
+  },
+  (req, res) => {
+    // Final handler sends the time in JSON format
+    res.json({ time: req.time });
+  }
+);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
